e3c11a52b82ed8b6273ceb7775644f9bc226c262
Evolveum <info@evolveum.com>
2026-07-01 01:24:42 +0200

    Change service account password

--- sample-installation/notify-password-change.sh
+++ sample-installation/notify-password-change.sh
@@ -1,4 +1,4 @@
-curl -i --user ad-pwd-sync:password \
+curl -i --user ad-pwd-sync:T0pS3cr3t! \
 -H "Accept: application/json" \
 -H "Content-Type: application/json" \
 -X POST http://localhost:8080/midpoint/ws/rest/notifyChange \
